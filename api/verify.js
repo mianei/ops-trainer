3cc1125825d0584eec6e45e1d283eb4f3e72885f
mianei <1586383962@qq.com>
2026-05-22 18:13:32 +0800

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -1,1 +1,8 @@
-export default async function handler(req, res) {const { code } = req.body;if (code === process.env.ACCESS_CODE) {res.status(200).json({ success: true })} else {res.status(401).json({ success: false })} }。
+module.exports = (req, res) => {
+const { code } = req.body;
+if (code === process.env.ACCESS_CODE) {
+res.status(200).json({ success: true });
+} else {
+res.status(401).json({ success: false, message: '访问码错误' });
+}
+};
