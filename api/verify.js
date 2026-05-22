bbdd1a0fe305c16ef7602ac9042476cfd02752a8
mianei <1586383962@qq.com>
2026-05-22 18:16:25 +0800

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -1,8 +1,16 @@
 module.exports = (req, res) => {
-const { code } = req.body;
-if (code === process.env.ACCESS_CODE) {
-res.status(200).json({ success: true });
-} else {
-res.status(401).json({ success: false, message: '访问码错误' });
-}
+  const body = req.body || {};
+  const { code } = body;
+
+  const validCode = process.env.ACCESS_CODE;
+
+  if (!code) {
+    return res.status(400).json({ success: false, msg: "未传入验证码" });
+  }
+
+  if (code === validCode) {
+    return res.status(200).json({ success: true });
+  } else {
+    return res.status(401).json({ success: false, msg: "验证码错误" });
+  }
 };
